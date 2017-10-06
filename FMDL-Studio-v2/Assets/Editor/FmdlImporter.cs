60bea510fafd58ce84cbc98ff9ad04d18266fb7f
Asduffo <Asduffo@users.noreply.github.com>
2017-10-06 22:19:52 +0200

    Update FmdlImporter.cs

--- FMDL-Studio-v2/Assets/Editor/FmdlImporter.cs
+++ FMDL-Studio-v2/Assets/Editor/FmdlImporter.cs
@@ -1,15 +1,15 @@
-﻿using System.IO;
+using System.IO;
 
 public static class FmdlImporter
 {
-    public static void FMDLStream(string path)
+    public static void FMDLStream(string path, bool shouldSave)
     {
         FileStream stream = new FileStream(path, FileMode.Open);
 
         Fmdl fmdl = new Fmdl(Path.GetFileNameWithoutExtension(path));
         UnityModel model = new UnityModel();
         fmdl.Read(stream);
-        model.GetDataFromFmdl(fmdl);
+        model.GetDataFromFmdl(fmdl, shouldSave);
         stream.Close();
     }
 }
