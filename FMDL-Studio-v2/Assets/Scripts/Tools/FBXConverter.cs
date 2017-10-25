f189461736273f31b1a187211e21797293ce7240
BobDoleOwndU <spikey5556@hotmail.com>
2017-10-25 19:28:23 -0400

    Fixed the FBXConverter not working with some language settings.

--- FMDL-Studio-v2/Assets/Scripts/Tools/FBXConverter.cs
+++ FMDL-Studio-v2/Assets/Scripts/Tools/FBXConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -31,6 +32,8 @@ public static class FBXConverter
 
     public static void ConvertToFBX(GameObject gameObject, string filePath)
     {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
         Clear();
 
         int numModelObjects = 1;
